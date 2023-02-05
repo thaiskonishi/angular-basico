68bfee1d7b00d9f30af8840c528e117f0aea7d33
ThaisMK <thais.konishi@gmail.com>
2023-02-05 15:17:40 -0300

    ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule
+       
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,17 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
-
   url:string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem='http://lorempixel.com.br/400/200/nature/';
 
+  valorAtual:string ='';
+  valorSalvo: string ='';
+  isMouseOver: boolean = false;
+  
+  pessoa: any = {
+    nome: 'def',
+    idade: 20
+  }
   getValor(){
     return 1;
   }
   getCurtirCurso() {
     return true
     }
+
+  botaoClicado(){
+    alert('o botão foi clicado')
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    // console.log(evento);
+    // console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual= (<HTMLInputElement>evento.target).value;
+  }
+
+  salvaValor(valor:any){
+    this.valorSalvo =  (<HTMLInputElement>valor.target).value;
+  }
+
+  onMouseOverOut(){
+  this.isMouseOver= !this.isMouseOver;
+  }
+
+  // getNome(evento:any){
+  //   return this.nome = (<HTMLInputElement>evento.target).value;
+  // }
   constructor(){}
   ngOnInit(): void {
       
