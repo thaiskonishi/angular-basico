c7c1802a1994d04a0a7a6b135208307c92fb8528
ThaisMK <thais.konishi@gmail.com>
2023-02-05 10:01:10 -0300

    inicio

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+
+  url:string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem='http://lorempixel.com.br/400/200/nature/';
+
+  getValor(){
+    return 1;
+  }
+  getCurtirCurso() {
+    return true
+    }
+  constructor(){}
+  ngOnInit(): void {
+      
+  }
+
+}
